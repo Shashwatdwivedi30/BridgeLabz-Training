a5c541784ffb86d0efa600b4ddd1156b282d5e88
Shashwatdwivedi30 <shashwatdwivedi3011@gmail.com>
2025-12-17 23:26:05 +0530

    17-12-25-gcr-codebase

---
+++ core-java-practice/gcr-codebase/programming-elements/SI.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        double r = sc.nextDouble();
+        double t =  sc.nextDouble();
+        double si = (p*r*t) / 100;
+        System.out.println("The SI is: "+si);
+    }
+}
