045e90255f89288c3f2f969f20d126fe2ae1ae68
Shashwat Dwivedi <shashwatdwivedi3011@gmail.com>
2025-12-19 10:49:36 +0530

    Delete core-java-practice/gcr-codebase/programming-elements/level1/VolOfEarth.java

--- core-java-practice/gcr-codebase/programming-elements/level1/VolOfEarth.java
+++
@@ -1,9 +0,0 @@
-public class VolOfEarth {
-    public static void main(String[] args) {
-        int km_radius = 6378;
-        double miles_radius = km_radius / 1.6;
-        double volume_km = (4.0/3) * 3.14 * Math.pow(km_radius, 3);
-        double volume_miles = (4.0/3) * 3.14 * Math.pow(miles_radius, 3);
-        System.out.println("The volume of earth in cubic kilometers is "+volume_km+" and cubic miles is "+volume_miles);
-    }
-}
