936e9b6202af578b590431bc31948781ec00c4ef
Shashwatdwivedi30 <shashwatdwivedi3011@gmail.com>
2026-02-04 22:00:49 +0530

    [Shashwat] added code of Lambda Function

--- core-java-practice/gcr-codebase/programming-elements/level1/DistanceConversion.java
+++
@@ -1,6 +0,0 @@
-public class DistanceConversion {
-    public static void main(String[] args) {
-        float km = 10.8f;
-        System.out.println("The distance "+km+" in miles is "+(10.8 * 0.6));
-    }
-}
